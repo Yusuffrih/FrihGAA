ca4d0c1c6cffd5f4ca6c313a02ee1fd934488353
Yusuffrih <yusuffrih@hotmail.com>
2021-11-07 14:39:24 +0000

    Added files to gitignore

--- home/views.py
+++ home/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def index(request):
+    """
+    The view that returns the index page
+    """
+
+    return render(request, 'home/index.html')
