52544b5aa244521ad3efb155a347a481a139573c
Yusuffrih <yusuffrih@hotmail.com>
2021-11-09 20:11:08 +0000

    Added templates to settings

--- frihgaa/settings.py
+++ frihgaa/settings.py
@@ -65,7 +65,10 @@ ROOT_URLCONF = 'frihgaa.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'templates'),
+            os.path.join(BASE_DIR, 'templates', 'allauth'),
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
