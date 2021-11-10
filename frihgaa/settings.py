0f34497b5ddd033ec1795677a59fe41ed0e5d38c
Yusuffrih <yusuffrih@hotmail.com>
2021-11-10 19:48:12 +0000

    Prepared to deploy to Heroku

--- frihgaa/settings.py
+++ frihgaa/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 import os
 from pathlib import Path
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -107,11 +108,15 @@ WSGI_APPLICATION = 'frihgaa.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.parse('postgres://jfggsdmofsnblq:c6f1e3d1e366e53d084a008bba78317df0c50026838eae3a695e3214cf577666@ec2-54-76-249-45.eu-west-1.compute.amazonaws.com:5432/da23frgtm0cv7c')
 }
 
 
