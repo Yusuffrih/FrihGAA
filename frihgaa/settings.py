2dd68310e6b167f8c5fd46b85cab2a5b333e4f01
Yusuffrih <yusuffrih@hotmail.com>
2021-11-10 21:59:29 +0000

    Added db & host settings to deploy to heroku

--- frihgaa/settings.py
+++ frihgaa/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-i4r7ye4v6zq6d_eu6lk+73vy#!o^e%irmoraoyvbwr$dpxpui3
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['ci-ms4-frihgaa.herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -108,12 +108,17 @@ WSGI_APPLICATION = 'frihgaa.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse('DATABASE_URL')
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 
 # Password validation
