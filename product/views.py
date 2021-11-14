c0e9a5ed7a0b94bd18d5bbc7dbf7a7689366c342
Yusuffrih <yusuffrih@hotmail.com>
2021-11-14 14:23:29 +0000

    Wired up products views & urls

--- product/views.py
+++ product/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+# from django.shortcuts import render (uncomment to use later)
+from django.views.generic import ListView
 
 # Create your views here.
+
+
+class Products(ListView):
+    """
+    The view that lists all memberships including search queries
+    """
+    template_name = 'product/products.html'
