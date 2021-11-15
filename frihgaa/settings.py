f4b9d31afb591496b2376bc224ab8fcb37b977d7
Yusuffrih <yusuffrih@hotmail.com>
2021-11-15 20:06:59 +0000

    Removed secret key & debug from settings

--- frihgaa/settings.py
+++ frihgaa/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-i4r7ye4v6zq6d_eu6lk+73vy#!o^e%irmoraoyvbwr$dpxpui3'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEVELOPMENT')
 
 ALLOWED_HOSTS = ['ci-ms4-frihgaa.herokuapp.com', 'localhost']
 
