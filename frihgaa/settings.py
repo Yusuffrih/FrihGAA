0cbe18296bd734eeaf4250a0d8b40e5c34254957
Yusuffrih <yusuffrih@hotmail.com>
2021-11-15 21:58:48 +0000

    Added delivery settings

--- frihgaa/settings.py
+++ frihgaa/settings.py
@@ -158,6 +158,10 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+# Stripe
+FREE_DELIVERY_THRESHOLD = 50
+STANDARD_DELIVERY_PERCENTAGE = 10
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
