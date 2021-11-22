22446da752a5dd1e91ab5bc69bf9a892ccbef23b
Yusuffrih <yusuffrih@hotmail.com>
2021-11-22 21:03:29 +0000

    Edited products and category model

--- product/models.py
+++ product/models.py
@@ -5,19 +5,45 @@ class Category(models.Model):
     class Meta:
         verbose_name_plural = 'Categories'
 
-    name = models.CharField(max_length=254)
-    friendly_name = models.CharField(max_length=254, null=True, blank=True)
+    name = models.CharField(
+        max_length=254
+        )
+    friendly_name = models.CharField(
+        max_length=254,
+        null=True,
+        blank=True
+        )
 
 
 class Product(models.Model):
-    category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
-    is_membership = models.BooleanField(default=False, null=True, blank=True)
-    name = models.CharField(max_length=254)
+    category = models.ForeignKey(
+        'Category',
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL
+        )
+    name = models.CharField(
+        max_length=254
+        )
     description = models.TextField()
-    has_sizes = models.BooleanField(default=False, null=True, blank=True)
-    price = models.DecimalField(max_digits=6, decimal_places=2)
-    image_url = models.URLField(max_length=1024, null=True, blank=True)
-    image = models.ImageField(null=True, blank=True)
+    has_sizes = models.BooleanField(
+        default=True,
+        null=True,
+        blank=True
+        )
+    price = models.DecimalField(
+        max_digits=6,
+        decimal_places=2
+        )
+    image_url = models.URLField(
+        max_length=1024,
+        null=True,
+        blank=True
+        )
+    image = models.ImageField(
+        null=True,
+        blank=True
+        )
 
     def __str__(self):
         return self.name
