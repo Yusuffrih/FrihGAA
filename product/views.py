c820d564ac77b6f8cbf312229ff953759ba88917
Yusuffrih <yusuffrih@hotmail.com>
2021-12-01 10:52:56 +0000

    Set up products page view & url

--- product/views.py
+++ product/views.py
@@ -1,13 +1,21 @@
 # from django.shortcuts import render (uncomment to use later)
-from django.views.generic import ListView
+from django.shortcuts import render
 from .models import Product
 
 # Create your views here.
 
 
-class Products(ListView):
+def all_products(request):
     """
-    The view that lists all memberships including search queries
+    A view to display all products in a list,
+    sorting,
+    search queries
     """
-    template_name = 'product/products.html'
-    queryset = Product.objects.all()
+
+    products = Product.objects.all()
+
+    context = {
+        'products': products,
+    }
+
+    return render(request, 'products/products.html', context)
