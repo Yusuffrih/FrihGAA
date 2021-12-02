53f1181477f6f616a6119042ae3abacf09767dd4
Yusuffrih <yusuffrih@hotmail.com>
2021-12-02 09:43:29 +0000

    Migrated changes to models

--- product/models.py
+++ product/models.py
@@ -14,6 +14,12 @@ class Category(models.Model):
         blank=True
         )
 
+    def __str__(self):
+        return self.name
+
+    def get_friendly_name(self):
+        return self.friendly_name
+
 
 class Product(models.Model):
     category = models.ForeignKey(
