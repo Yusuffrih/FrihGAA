93d3af802d7cf87a5b207d942fd5a673298f0845
Yusuffrih <yusuffrih@hotmail.com>
2021-12-05 13:07:34 +0000

    Added basic checkout view

--- checkout/views.py
+++ checkout/views.py
@@ -1,11 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse
+from django.contrib import messages
 
-# Create your views here.
+from .forms import OrderForm
 
 
 def checkout(request):
-    """
-    The view that handles checkouts
-    """
+    basket = request.session.get('basket', {})
+    if not basket:
+        messages.error(request, "There's nothing in your basket at the moment")
+        return redirect(reverse('products'))
 
-    return render(request, 'checkout/checkout.html')
+    order_form = OrderForm()
+    template = 'checkout/checkout.html'
+    context = {
+        'order_form': order_form,
+    }
+
+    return render(request, template, context)
