06755bdc7add31e17a330ccd69f4b98059af0704
Yusuffrih <yusuffrih@hotmail.com>
2021-12-07 15:17:26 +0000

    Deleted whitespace on profile view

--- profile/views.py
+++ profile/views.py
@@ -3,5 +3,9 @@ from django.shortcuts import render
 
 def profile(request):
     """ Display the user's profile """
+    template = 'profile/profiles.html'
+    context = {
 
-    return render(request, "profile/profile.html")
+    }
+
+    return render(request, template, context)
