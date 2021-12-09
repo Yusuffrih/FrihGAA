64f53062461cb36dab6339806050e54813b0badb
Yusuffrih <yusuffrih@hotmail.com>
2021-12-09 15:31:20 +0000

    Fixed debug setting

--- frihgaa/settings.py
+++ frihgaa/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DEVELOPMENT')
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['ci-ms4-frihgaa.herokuapp.com', 'localhost']
 
