be7fdb91993bb7f0e4e5363cb46a19a0e7a1dfe8
Yusuffrih <yusuffrih@hotmail.com>
2021-12-11 17:05:13 +0000

    Changed fields to text instead of char

--- faq/models.py
+++ faq/models.py
@@ -7,8 +7,8 @@ class Faq(models.Model):
     class Meta:
         verbose_name_plural = 'FAQs'
 
-    question = models.CharField(max_length=300, null=False, blank=False)
-    answer = models.CharField(max_length=900, null=False, blank=False)
+    question = models.TextField(max_length=300, null=False, blank=False)
+    answer = models.TextField(max_length=900, null=False, blank=False)
 
     def __str__(self):
         return self.question
