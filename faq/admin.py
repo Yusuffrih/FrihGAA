0804f0dd8e2c39b19bb7693b2fa73cded3104e6c
Yusuffrih <yusuffrih@hotmail.com>
2021-12-11 11:12:53 +0000

    Set up model on admin site

--- faq/admin.py
+++ faq/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Faq
 
 # Register your models here.
+
+
+class FaqAdmin(admin.ModelAdmin):
+    list_display = (
+        'question',
+        'answer',
+    )
+
+
+admin.site.register(Faq, FaqAdmin)
