cbbad16cd57cf54b492c455aa9e77dc23e732016
Yusuffrih <yusuffrih@hotmail.com>
2021-12-12 14:27:06 +0000

    Set up contact app model

---
+++ contact/models.py
@@ -0,0 +1,31 @@
+from django.db import models
+
+# Create your models here.
+
+
+class Contact(models.Model):
+    verbose_name = 'Contact'
+
+    name = models.CharField(
+        max_length=50,
+        null=False,
+        blank=False
+        )
+    email = models.EmailField(
+        max_length=50,
+        null=False,
+        blank=False
+        )
+    subject = models.CharField(
+        max_length=254,
+        null=False,
+        blank=False
+        )
+    content = models.TextField(
+        max_length=3000,
+        null=False,
+        blank=False
+        )
+
+    def __str__(self):
+        self.name
