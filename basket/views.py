70f14a07af237c51c855f2e2f48c2e3009df5a4e
Yusuffrih <yusuffrih@hotmail.com>
2021-12-16 16:02:40 +0000

    Fixed lie that was too long

--- basket/views.py
+++ basket/views.py
@@ -1,4 +1,7 @@
-from django.shortcuts import render, redirect, reverse, HttpResponse, get_object_or_404
+from django.shortcuts import (
+    render, redirect, reverse,
+    HttpResponse, get_object_or_404
+    )
 from django.contrib import messages
 from product.models import Product
 
